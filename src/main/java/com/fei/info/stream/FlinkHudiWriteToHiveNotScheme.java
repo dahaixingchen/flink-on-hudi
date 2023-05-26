d5f8daca32234a8e61177fe48f91a862a197d224
dahaixingchen <xingchendahai_fei@163.com>
2023-05-26 09:29:43 +0800

    测试hudi表结构的变更

--- src/main/java/com/fei/info/stream/FlinkHudiWriteToHiveNotScheme.java
+++ src/main/java/com/fei/info/stream/FlinkHudiWriteToHiveNotScheme.java
@@ -21,7 +21,7 @@ import java.util.Properties;
  * @Author chengfei
  * @DateTime 2023/5/23 17:40
  **/
-public class FlinkHudiWriteToHive {
+public class FlinkHudiWriteToHiveNotScheme {
     public static void main(String[] args) throws Exception {
         System.setProperty("HADOOP_USER_NAME", "hdfs");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -47,19 +47,22 @@ public class FlinkHudiWriteToHive {
             genericRowData.setField(2, StringData.fromString(String.valueOf(p.getAge())));
             genericRowData.setField(3, StringData.fromString(String.valueOf(p.getDt())));
             genericRowData.setField(4, StringData.fromString(p.getPt()));
+//            genericRowData.setField(5, StringData.fromString("666666"));
             return genericRowData;
         });
         dStremMor.print();
 
-        HoodiePipeline.Builder builderMor = HoodiePipeline.builder("flink_hudi_mor_stream_2511")
+        HoodiePipeline.Builder builderMor = HoodiePipeline.builder("flink_hudi_mor_stream_2515")
                 .column("uuid VARCHAR(20)")
-                .column("name VARCHAR(10)")
-                .column("age VARCHAR(10)")
+//                .column("name VARCHAR(10)")
+//                .column("age VARCHAR(10)")
                 .column("dt VARCHAR(20)")
                 .column("pt VARCHAR(20)")
                 .partition("pt")
                 .pk("uuid")
-                .option(FlinkOptions.PATH.key(), "/user/chengfei/hudi-flink-mor-stream")
+
+                //添加参数
+                .option(FlinkOptions.PATH.key(), "/user/chengfei/hudi-flink-mor-stream15")
 //                .option(FlinkOptions.TABLE_TYPE.key(), HoodieTableType.COPY_ON_WRITE.name())
                 .option(FlinkOptions.TABLE_TYPE.key(), HoodieTableType.MERGE_ON_READ.name())
                 .option(FlinkOptions.COMPACTION_TASKS.key(), 1)
@@ -73,52 +76,13 @@ public class FlinkHudiWriteToHive {
                 .option(FlinkOptions.READ_AS_STREAMING.key(), true)
                 .option(FlinkOptions.HIVE_SYNC_METASTORE_URIS.key(), "thrift://cdh-7253:9083")
                 .option(FlinkOptions.HIVE_SYNC_JDBC_URL.key(), "jdbc:hive2://cdh-7253:10000")
-                .option(FlinkOptions.HIVE_SYNC_TABLE.key(), "flink_hudi_mor_stream_2511")
+                .option(FlinkOptions.HIVE_SYNC_TABLE.key(), "flink_hudi_mor_stream_2515")
                 .option(FlinkOptions.HIVE_SYNC_DB.key(), "hudi_db")
                 .option(FlinkOptions.HIVE_SYNC_USERNAME.key(), "ykas_aq")
                 .option(FlinkOptions.HIVE_SYNC_PASSWORD.key(), "XwHdDvzwLRrdKvM3")
                 .option(FlinkOptions.READ_STREAMING_CHECK_INTERVAL.key(), 4);
 
         builderMor.sink(dStremMor, false);
-
-
-        //cow表实现表结构跟mor表不一样，同时能写入数据
-        DataStream<RowData> dStreamCow = dStream.map(p -> {
-
-            GenericRowData genericRowData = new GenericRowData(5);
-            genericRowData.setField(0, StringData.fromString(p.getUuid()));
-            genericRowData.setField(1, StringData.fromString(p.getName()));
-            genericRowData.setField(2, StringData.fromString(String.valueOf(p.getAge())));
-            genericRowData.setField(3, StringData.fromString(String.valueOf(p.getDt())));
-            genericRowData.setField(4, StringData.fromString(p.getPt()));
-            return genericRowData;
-        });
-        HoodiePipeline.Builder builderCow = HoodiePipeline.builder("flink_hudi_cow_stream_2511")
-                .column("uuid VARCHAR(20)")
-                .column("name VARCHAR(10)")
-                .column("age VARCHAR(10)")
-                .column("dt VARCHAR(20)")
-                .column("pt VARCHAR(20)")
-                .partition("pt")
-                .pk("uuid")
-                .pk("")
-                .option(FlinkOptions.PATH.key(), "/user/chengfei/hudi-flink-stream")
-                .option(FlinkOptions.TABLE_TYPE.key(), HoodieTableType.COPY_ON_WRITE.name())
-                .option(FlinkOptions.COMPACTION_TASKS.key(), 1)
-                .option(FlinkOptions.WRITE_TASKS.key(), 1)
-                .option(FlinkOptions.PRECOMBINE_FIELD.key(), "dt") //当主键冲突的时候以什么字段的最大值为标准
-        //自动开启创建表
-                .option(FlinkOptions.HIVE_SYNC_ENABLED.key(), true)
-                .option(FlinkOptions.READ_AS_STREAMING.key(), true)
-                .option(FlinkOptions.HIVE_SYNC_METASTORE_URIS.key(), "thrift://cdh-7253:9083")
-                .option(FlinkOptions.HIVE_SYNC_JDBC_URL.key(), "jdbc:hive2://cdh-7253:10000")
-                .option(FlinkOptions.HIVE_SYNC_TABLE.key(), "flink_hudi_cow_stream_2511")
-                .option(FlinkOptions.HIVE_SYNC_DB.key(), "hudi_db")
-                .option(FlinkOptions.HIVE_SYNC_USERNAME.key(), "ykas_aq")
-                .option(FlinkOptions.HIVE_SYNC_PASSWORD.key(), "XwHdDvzwLRrdKvM3")
-                .option(FlinkOptions.READ_STREAMING_CHECK_INTERVAL.key(), 4);
-
-        builderCow.sink(dStreamCow, false);
 
         env.execute();
     }
